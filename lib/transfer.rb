2b0fd51d26f3869b960bd0171dbe5b220a4da8b6
Jim Hough <teachlife@outlook.com>
2019-05-04 01:13:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
     end
   end
   def reverse_transfer
-    if @status = 'complete'
+    if @status == 'complete'
       @sender.balance += @amount
       @receiver.balance += @amount
       @status = 'reversed'
