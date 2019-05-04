872ebf8ab705a7a129ccac094aa114d51fe20cf4
Jim Hough <teachlife@outlook.com>
2019-05-04 00:59:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,16 +13,21 @@ class Transfer
     @sender.valid? && @receiver.valid? && @sender.balance > @amount
   end
   def execute_transaction
-    if self.valid?
+    if self.valid? && @status = 'pending'
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = 'complete'
-      @amount = 0
-
     else
       @status = 'rejected'
       "Transaction rejected. Please check your account balance."
     end
   end
-
+  def reverse_transfer
+    if @status = 'complete'
+      @sender.balance += @amount
+      @receiver.balance += @amount
+      @status = 'reversed'
+    end
+  end
+      
 end
