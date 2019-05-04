af42f42a06313d27268c3165a8f1934ed8a66058
Jim Hough <teachlife@outlook.com>
2019-05-04 00:39:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @amount = 0
     else
       @status = 'rejected'
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     end
   end
 
